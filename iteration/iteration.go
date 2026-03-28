9ed120d1da0312396bd3a25a785acca49f493e92
gabrielnsil <gabrielnsil@outlook.com>
2026-03-28 17:09:21 -0300

    test: added new subtests to Repeat refactor: refactored Repeat to receive the number of times that a character could be repeated.wq

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -2,10 +2,14 @@ package iteration
 
 import "strings"
 
-const repeatCount = 5
+func Repeat(character string, repeatCount int) string {
 
-func Repeat(character string) string {
 	var repeated strings.Builder
+	
+	if repeatCount < 1 {
+		return character
+	}
+
 	for i := 0; i < repeatCount; i++ {
 		repeated.WriteString(character)
 	}
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -4,16 +4,31 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
 
-	if repeated != expected {
+	t.Run("testing if the character repeats 10 times", func(t *testing.T) {
+		repeated := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
+		AssertCorrectRepetition(t, repeated, expected)
+	})
+
+	t.Run ("testing if the function defaults to character if 'times' < 1", func (t *testing.T) {
+		repeated := Repeat("b", -1)
+		expected := "b"
+		AssertCorrectRepetition(t, repeated, expected)
+	})
+}
+
+
+func AssertCorrectRepetition(t testing.TB, repeated, expected string) {
+	t.Helper()
+	if expected != repeated {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
+
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 10)
 	}
 }
